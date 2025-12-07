0ae8cea90e3408411b67da3abb97ff51829d0c20
Галкин Владимир <galkin@irp.ru>
2025-12-07 20:13:53 +0300

    Update to use stat

--- timestamp.sh
+++ timestamp.sh
@@ -3,7 +3,7 @@
 find /var/log/ -name "*.log" -type f | while read file; do
     dir=$(dirname "$file")
     base=$(basename "$file" .log)
-    timestamp=$(date +%Y%m%d_%H%M%S)
-    mv "$file" "$dir/${base}_${timestamp}.log"
+    timestamp=$(stat -c '%Y' $file)
+    mv "$file" "$dir/${base}_{${timestamp}}.log"
 done
 
