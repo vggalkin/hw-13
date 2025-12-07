2d4bcbd0b84f47a8fdf37e6f7ddf55149b0bbe6c
Галкин Владимир <galkin@irp.ru>
2025-12-07 22:52:47 +0300

    Fix

--- timestamp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find /var/log/ -name "*.log" -type f | while read file; do
-    dir=$(dirname "$file")
-    base=$(basename "$file" .log)
-    timestamp=$(stat -c '%Y' $file)
-    mv "$file" "$dir/${base}_{${timestamp}}.log"
-done
-
